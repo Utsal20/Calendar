f2f0cd70df6124d284e88b3b7bd28e9e984cd6da
igauli20 <igauli20@my.whitworth.edu>
2017-05-09 13:54:05 -0700

    Add files via upload
    Display window's size is limited
    X,Y positions of left button up
    Text field is added
    Date Time field added

---
+++ Calendar-master/Calendar/Calendar/Day.h
@@ -0,0 +1,30 @@
+
+
+#ifndef DAY_H
+#define DAY_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+class Day {
+private:
+	std::string fileName;
+	std::vector <std::string> eventTime;
+	std::vector <std::string> eventName;
+public:
+	//stores the event into the vector and updates the file
+	void addEvent(std::string time, std::string name);
+
+	//once the day is clicked, this function returns the events for the day, displays them on the screen and assigns them to eventDetail for faster access
+	void checkEvent(int opt);
+
+	//deletes an event (removes the item from the vector and updates the file)
+	void deleteEvent(std::string time, std::string name);
+
+	//writes everything to a file
+	void writeToFile();
+};
+#endif // !DAY_H
+
---
+++ Calendar-master/Calendar/Calendar/DisplayMgr.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef DISPLAYMGR_H
+#define DISPLAYMGR_H
+
+#include "stdafx.h"
+#include <CommDlg.h>
+
+void PlannerReset();
+
+void PlannerDrawWindow(HWND hwnd, HDC hdc);
+
+#endif // !
